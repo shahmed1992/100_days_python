30c7ff5316d0bb4387be95d912f82a6fd92645d0
shahmed1992 <sh.ahmed1992@gmail.com>
2024-11-16 18:48:30 +0530

    Day 26

--- Day_26/Coding_Exercise_Day_26.py
+++ Day_26/Coding_Exercise_Day_26.py
@@ -109,7 +109,27 @@ def coding_exercise_18():
     print(words_dict)
 
 
+# Coding Exercise 19 Dictionary Comprehension 2
+"""
+Dictionary Comprehension 2
+You are going to use Dictionary Comprehension to create a dictionary called weather_f that takes each temperature in degrees Celsius and converts it into degrees Fahrenheit. 
+
+
+
+To convert temp_c into temp_f use this formula: 
+
+(temp_c * 9/5) + 32 = temp_f
+"""
+
+
+def coding_exercise_19():
+    weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+    weather_f = {day: round((temp_c*(9/5)), 2)+32 for day, temp_c in weather_c.items()}
+    print(weather_f)
+
+
 coding_exercise_15()
 coding_exercise_16()
 data_overlap()
 coding_exercise_18()
+coding_exercise_19()
---
+++ Day_26/dict_data_frame.py
@@ -0,0 +1,9 @@
+import pandas
+student_dict = {
+
+    "student": ["Hussain", "Umair", "Shahika"],
+    "score": [56, 76, 98]
+}
+print(student_dict)
+print(pandas.dataframe(student_dict))
+
